b33997c4b8948a3f143d1180d6f1a7ce83534ec7
Sayan Mondal <sayanmondal2098@gmail.com>
2019-12-26 23:02:28 +0530

    basic genetic script added

--- Genetic.py
+++ Genetic.py
@@ -67,21 +67,225 @@ def create_starting_population(size, the_map):
         
     return population
 
+
+def fitness(route, the_map):
     
-def fitness(member):
-    return True
+    score = 0
+    
+    for i in range(1, len(route)):
+        if (the_map[route[i-1]][route[i]] == 0) and i != len(the_map)-1:
+            print("WARNING: INVALID ROUTE")
+            print(route)
+            print(the_map)
+        score = score + the_map[route[i-1]][route[i]]
 
-def crossover(a,b):
-    return a
+    return score
 
-def mutation(member):
-    return member
+def crossover(a, b):
+        
+    # I initially made an error here by allowing routes to crossover at any point, which obviously won't work
+    # you have to insure that when the two routes cross over that the resulting routes produce a valid route
+    # which means that crossover points have to be at the same position value on the map
+    
+    common_elements = set(a) & set(b)
+    
+    if len(common_elements) == 2:
+        return (a, b)
+    else:
+        common_elements.remove(0)
+        common_elements.remove(max(a)) 
+        value = random.sample(common_elements, 1)        
+    
+    cut_a = np.random.choice(np.where(np.isin(a, value))[0])
+    cut_b = np.random.choice(np.where(np.isin(b, value))[0])
+    
+    new_a1 = copy.deepcopy(a[0:cut_a])
+    new_a2 = copy.deepcopy(b[cut_b:])
+    
+    new_b1 = copy.deepcopy(b[0:cut_b])
+    new_b2 = copy.deepcopy(a[cut_a:])
+    
+    new_a = np.append(new_a1, new_a2)
+    new_b = np.append(new_b1, new_b2)
+       
+    return (new_a, new_b)
 
-def create_new_member():
-    return member
+
+def mutate(route, probability, the_map):
+    
+    new_route = copy.deepcopy(route)
+    
+    for i in range(1, len(new_route)):
+        if random.random() < probability:
+            
+            go = True
+
+            while go:
+
+                possible_values = np.nonzero(the_map[new_route[i-1]])
+                proposed_value = random.randint(0,len(possible_values[0])-1)
+                route = np.append(new_route, possible_values[0][proposed_value])
+
+                if new_route[i] == len(the_map)-1:
+                    go = False
+                else:
+                    i += 1
+    
+    return new_route
+
+
+def create_new_member(the_map):
+    # here we are going to create a new route
+    # the new route can have any number of steps, so we'll select that randomly
+    # the structure of the route will be a vector of integers where each value is the next step in the route
+    # Everyone starts at 0, so the first value in the vector will indicate where to attempt to go next.
+    # That is, if v_i = 4, then that would correspond to X_0,4 in the map that was created at initialization
+    
+    # N is the size of the map, so we need to make sure that 
+    # we don't generate any values that exceed the size of the map
+
+    N = len(the_map)
+    
+    route = np.zeros(1, dtype=int)
+
+    go = True
+    
+    i = 1
+    
+    while go:
+        
+        possible_values = np.nonzero(the_map[route[i-1]])
+        proposed_value = random.randint(0,len(possible_values[0])-1)
+        route = np.append(route, possible_values[0][proposed_value])
+                
+        if route[i] == N-1:
+            go = False
+        else:
+            i += 1
+    
+    return route
+
+
+def score_population(population, the_map):
+    
+    scores = []
+    
+    for i in range(0, len(population)):
+        scores += [fitness(population[i], the_map)]
+        
+    return scores
+
+
+
+def pick_mate(scores):
+
+    array = np.array(scores)
+    temp = array.argsort()
+    ranks = np.empty_like(temp)
+    ranks[temp] = np.arange(len(array))
+
+    fitness = [len(ranks) - x for x in ranks]
+    
+    cum_scores = copy.deepcopy(fitness)
+    
+    for i in range(1,len(cum_scores)):
+        cum_scores[i] = fitness[i] + cum_scores[i-1]
+        
+    probs = [x / cum_scores[-1] for x in cum_scores]
+    
+    rand = random.random()
+    
+    for i in range(0, len(probs)):
+        if rand < probs[i]:
+            
+            return i
+
 
 def create_next_generation(population):
     return population
 
-def main(number_of_iteration):
-    return True
+
+
+
+
+
+
+
+def main():
+    
+    # parameters
+    sparseness_of_map = 0.95
+    size_of_map = 1000
+    population_size = 30
+    number_of_iterations = 1000
+    number_of_couples = 9
+    number_of_winners_to_keep = 2
+    mutation_probability = 0.05
+    number_of_groups = 1
+    
+    # initialize the map and save it
+    the_map = initialize_complex_map(sparseness_of_map, size_of_map, number_of_groups)
+
+    # create the starting population
+    population = create_starting_population(population_size, the_map)
+
+    last_distance = 1000000000
+    # for a large number of iterations do:
+        
+    for i in range(0,number_of_iterations):
+        new_population = []
+        
+        # evaluate the fitness of the current population
+        scores = score_population(population, the_map)
+
+        best = population[np.argmin(scores)]
+        number_of_moves = len(best)
+        distance = fitness(best, the_map)
+        
+        if distance != last_distance:
+            print('Iteration %i: Best so far is %i steps for a distance of %f' % (i, number_of_moves, distance))
+            plot_best(the_map, best, i)
+
+        
+        # allow members of the population to breed based on their relative score; 
+            # i.e., if their score is higher they're more likely to breed
+        for j in range(0, number_of_couples):  
+            new_1, new_2 = crossover(population[pick_mate(scores)], population[pick_mate(scores)])
+            new_population = new_population + [new_1, new_2]
+  
+        # mutate
+        for j in range(0, len(new_population)):
+            new_population[j] = np.copy(mutate(new_population[j], 0.05, the_map))
+            
+        # keep members of previous generation
+        new_population += [population[np.argmin(scores)]]
+        for j in range(1, number_of_winners_to_keep):
+            keeper = pick_mate(scores)            
+            new_population += [population[keeper]]
+            
+        # add new random members
+        while len(new_population) < population_size:
+            new_population += [create_new_member(the_map)]
+            
+        #replace the old population with a real copy
+        population = copy.deepcopy(new_population)
+                
+        last_distance = distance
+        
+    # plot the results
+
+
+
+def plot_best(the_map, route, iteration_number):
+    ax = sns.heatmap(the_map)
+
+    x=[0.5] + [x + 0.5 for x in route[0:len(route)-1]] + [len(the_map) - 0.5]
+    y=[0.5] + [x + 0.5 for x in route[1:len(route)]] + [len(the_map) - 0.5]
+    
+    plt.plot(x, y, marker = 'o', linewidth=4, markersize=12, linestyle = "-", color='white')
+    plt.savefig('images/new1000plot_%i.png' %(iteration_number), dpi=300)
+    plt.show()
+
+
+
+main()
